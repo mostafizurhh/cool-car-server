8dc62c28997851714a780620be1f8d564ecf8053
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-03 20:57:01 +0000

    API created to load all service data and specific service data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config() /* to hide DB credential */
 
 app.use(cors())
@@ -23,8 +23,30 @@ app.listen(port, () => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('cool-car').collection('services');
+
+        /* to show DB's data to UI */
+        /* API to get all services */
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        /* API to get a specific service/data */
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
