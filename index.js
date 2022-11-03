2a63579b1383e17d6a1c8dde753b7cc77a0016e6
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-03 19:37:10 +0000

    .env file added to hide credential

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config() /* to hide DB credential */
 
 app.use(cors())
 app.use(express.json())
-
 
 app.get('/', (req, res) => {
     res.send('server is running')
@@ -14,3 +15,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`listening from port ${port}`)
 })
+
+/*--------------------
+  mongoDB connection 
+  -------------------*/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
